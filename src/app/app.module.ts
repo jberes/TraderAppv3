dc97792d408a1fbf423358850e4aba15cf43a003
Jason Beres <jasonb@infragistics.com>
2023-05-03 23:00:13 -0400

    Updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { IgxInputGroupModule, IgxIconModule, IgxListModule, IgxButtonModule, IgxRippleModule, IgxCardModule, IgxAvatarModule, IgxNavbarModule, IgxNavigationDrawerModule } from 'igniteui-angular';
-import { IgxCategoryChartModule } from 'igniteui-angular-charts';
+import { IgxCategoryChartModule, IgxFinancialChartModule } from 'igniteui-angular-charts';
 import { FormsModule } from '@angular/forms';
 import { DashboardsComponent } from './dashboards/dashboards.component';
 import { DashboardviewComponent } from './dashboardview/dashboardview.component';
@@ -36,6 +36,7 @@ import { HttpClientModule } from '@angular/common/http';
     IgxAvatarModule,
     IgxNavbarModule,
     IgxNavigationDrawerModule,
+    IgxFinancialChartModule,
     HttpClientModule
   ],
   providers: [],
